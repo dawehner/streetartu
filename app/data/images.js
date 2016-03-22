c82c9263a94f48586793fde1e7bb3bd019dc7019
Daniel Wehner <daniel@tag1consulting.com>
2016-03-22 17:16:20 -0500

    add data image js

---
+++ app/data/images.js
@@ -0,0 +1,10 @@
+var images = [
+  {filename: 'IMG_20151219_160257.jpg', id: 1},
+  {filename: 'IMG_20151224_081702.jpg', id: 2},
+  {filename: 'IMG_20151224_081736.jpg', id: 3},
+  {filename: 'IMG_20151219_160303.jpg', id: 4},
+  {filename: 'IMG_20151224_081712.jpg', id: 5},
+  {filename: 'IMG_20151224_082023.jpg', id: 6}
+];
+
+module.exports = images;
