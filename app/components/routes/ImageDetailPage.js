c8462f10f0f47627da01f56dc5cdec16bc692402
Daniel Wehner <daniel@tag1consulting.com>
2016-03-22 17:11:36 -0500

    Allow to use images in ImageDetailPage.js

--- app/components/routes/ImageDetailPage.js
+++ app/components/routes/ImageDetailPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from '../Header.js';
+import images from '../../data/images.js';
 
 class ImageDetailPage extends React.Component {
   render() {
     var imageId = this.props.params.imageId;
-    console.log(window.images);
+    var image = images[imageId];
 
     return <div><Header />
       <h2>Details</h2>
+      <h3>Filename</h3>
+      <code>{image.filename}</code>
+      <h3>Image ID</h3>
+      <code>{image.id}</code>
     </div>
   }
 }
--- app/main.js
+++ app/main.js
@@ -13,14 +13,7 @@ import App from './App.js';
 
 const history = createBrowserHistory();
 
-var images = [
-  {filename: 'IMG_20151219_160257.jpg', id: 1},
-  {filename: 'IMG_20151224_081702.jpg', id: 2},
-  {filename: 'IMG_20151224_081736.jpg', id: 3},
-  {filename: 'IMG_20151219_160303.jpg', id: 4},
-  {filename: 'IMG_20151224_081712.jpg', id: 5},
-  {filename: 'IMG_20151224_082023.jpg', id: 6}
-];
+import images from './data/images.js';
 
 class StreetartTuAppWrapper extends React.Component {
   render() {
